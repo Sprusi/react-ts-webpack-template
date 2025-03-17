53646cc194ce87c9f226fb72b450990f8c7e3106
brovko.dr <brovko@atb.su>
2025-03-17 22:34:07 +1000

    br-add-linter - added old vertion eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import Navigation from "./navigation/Navigation";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Navigation from './navigation/Navigation';
 
 export const App = () => {
   return (
--- src/components/default/Default.tsx
+++ src/components/default/Default.tsx
@@ -1,36 +1,15 @@
-import React, { useState } from "react";
+import { useState } from 'react';
+
+import styles from './Default.module.scss';
 
 export const Default = () => {
   const [count, setCount] = useState(0);
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "15%",
-        margin: "0 auto",
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
+    <div className={styles.defaultWrapper}>
+      <div className={styles.defaultContent}>
         <h1>{count}</h1>
-        <button
-          style={{
-            padding: "1em 3em",
-            borderRadius: "5px",
-            border: "1px solid grey",
-            color: "white",
-            backgroundColor: "#171717",
-          }}
-          onClick={() => setCount((prev) => prev + 1)}
-        >
-          +
+        <button className={styles.defaultContentButton} onClick={() => setCount((prev) => prev + 1)}>
+          {'+'}
         </button>
       </div>
     </div>
